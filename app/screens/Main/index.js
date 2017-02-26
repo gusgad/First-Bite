373a32d77a27a2e18daf1c3695253ee6760dd127
gusgadirov <gusgadirov@gmail.com>
2017-02-27 00:57:46 +0200

    Add button and change bg

--- app/screens/Main/index.js
+++ app/screens/Main/index.js
@@ -1,48 +1,56 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import SearchBar from './Components/SearchBar';
 var Dimensions = require('Dimensions');
 var {width, height} = Dimensions.get('window');
 
 export default class Main extends Component {
+  constructor(props) {
+      super(props);
+      this.onPressLearnMore = this.onPressLearnMore.bind(this);
+  }
+  onPressLearnMore() {
+        
+  }
   render() {
     return (
         <View style={styles.container}>
-            <Image source={require('./background.jpg')} style={styles.background} />
-            <View style={styles.searchView}>
-                <SearchBar style={styles.searchbar} />
+            <View style={styles.logoContainer}>
+                <Text style={styles.logo}>FirstBite</Text>
+                <SearchBar style={styles.searchBar} />
             </View>
+            <Button title='Find' style={styles.findButton} onPress={this.onPressLearnMore} color="#FBC02D"></Button>
         </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1
-  },
-  background: {
-      flex: 1,
-      resizeMode: "stretch",
-      width: width,
-      height: height,
-      opacity: 0.5,
-      position: 'absolute'
-  },
-  searchView: {
-      width: width,
-      height: height,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      justifyContent: 'center',
-      alignItems: 'center'
-  },
-  searchbar: {
-      width: 10,
-      height: 20,
-      backgroundColor: 'red'
-  }
+    container: {
+        flex: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+        justifyContent: 'space-around',
+        paddingTop: 30,
+        paddingBottom: 30,
+        backgroundColor: '#AD1457'
+    },
+    logoContainer: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    logo: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        fontFamily: 'Roboto',
+        color: '#FBC02D'
+    },
+    searchBar: {
+        fontFamily: 'Roboto',
+        color: 'white'
+    },
+    findButton: {
+        flex: 1
+    }
 });
